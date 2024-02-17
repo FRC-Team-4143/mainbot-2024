e74b733edf7a4155f3f07138386915b3bb0ae3e6
JoeyD2024 <150205129+JoeyD2024@users.noreply.github.com>
2024-02-17 15:51:22 -0600

    made new pose and odometry

--- src/main/java/frc/robot/subsystems/PoseEstimator.java
+++ src/main/java/frc/robot/subsystems/PoseEstimator.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.GenericSubscriber;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
@@ -14,8 +15,7 @@ import frc.lib.subsystem.Subsystem;
 public class PoseEstimator extends Subsystem {
 
     private static PoseEstimator instance;
-    private StructPublisher<Pose2d> pose_pub;
- 
+    private StructPublisher<Pose2d> pose_pub_;
 
     public static PoseEstimator getInstance() {
         if (instance == null) {
@@ -27,22 +27,27 @@ public class PoseEstimator extends Subsystem {
     private PeriodicIo io_;
     private Field2d field;
     private SwerveDrivePoseEstimator odometry;
+    private SwerveDrivePoseEstimator vision_filtered_odometry;
+    private GenericSubscriber vision_subsciber;
 
     PoseEstimator() {
         field = new Field2d();
         io_ = new PeriodicIo();
-        pose_pub = NetworkTableInstance.getDefault().getStructTopic("robot_pose", Pose2d.struct).publish();
+        pose_pub_ = NetworkTableInstance.getDefault().getStructTopic("robot_pose", Pose2d.struct).publish();
+        
     }
 
     @Override
     public void readPeriodicInputs(double timestamp) {
-    
-    }
 
+    }
+    // Make a subscriber, integate vision measurements wpilib method on the new odometry, getLastChange?
     @Override
     public void updateLogic(double timestamp) {
         var drive = SwerveDrivetrain.getInstance();
         io_.pose_ = odometry.update(drive.getImuYaw(), drive.getModulePositions());
+        
+        
     }
 
     @Override
@@ -52,14 +57,16 @@ public class PoseEstimator extends Subsystem {
     @Override
     public void outputTelemetry(double timestamp) {
         field.setRobotPose(io_.pose_);
-        SmartDashboard.putData("Field", field);
-        pose_pub.set(io_.pose_);
+        SmartDashboard.putData("Field", field); // TODO: Smartdashboard currently uses odometry pose
+        pose_pub_.set(io_.pose_);
     }
 
     @Override
     public void reset() {
         odometry = new SwerveDrivePoseEstimator(SwerveDrivetrain.getInstance().kinematics, new Rotation2d(),
                 SwerveDrivetrain.getInstance().getModulePositions(), new Pose2d());
+        vision_filtered_odometry = new SwerveDrivePoseEstimator(SwerveDrivetrain.getInstance().kinematics, null, null,
+                getVisionFilteredPose());
         io_ = new PeriodicIo();
     }
 
@@ -72,21 +79,28 @@ public class PoseEstimator extends Subsystem {
      * 
      * @return
      */
-    public Pose2d getRobotPose(){
+    public Pose2d getRobotPose() {
         return io_.pose_;
+    }
+
+    public Pose2d getVisionFilteredPose() {
+        return io_.vision_filtered_pose_;
     }
 
     /**
      * Resets the robot odom and set the robot pose to supplier Pose
+     * 
      * @param pose pose to update Robot pose to
      */
-    public void setRobotOdometry(Pose2d pose){
+    public void setRobotOdometry(Pose2d pose) {
         var drive = SwerveDrivetrain.getInstance();
         odometry.resetPosition(drive.getImuYaw(), drive.getModulePositions(), pose);
+        vision_filtered_odometry.resetPosition(drive.getImuYaw(), drive.getModulePositions(), pose);
     }
 
     class PeriodicIo extends LogData {
         Pose2d pose_ = new Pose2d();
+        Pose2d vision_filtered_pose_ = new Pose2d();
     }
 
 }
