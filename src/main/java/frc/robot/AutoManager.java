ec4ee1f375f7dfef211a26bd927dfc61b6d74553
ColeHunt <huntcr2001@gmail.com>
2024-02-26 08:20:06 -0600

    reworks autos to use constant aiming strat

--- src/main/java/frc/robot/AutoManager.java
+++ src/main/java/frc/robot/AutoManager.java
@@ -5,9 +5,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.SwerveDrivetrain;
-import frc.robot.subsystems.ShooterSubsystem.ShootTarget;
 import com.pathplanner.lib.auto.NamedCommands;
 import frc.robot.commands.*;
 
@@ -28,9 +26,8 @@ public class AutoManager {
     private final SendableChooser<Command> autoChooser;
 
     private AutoManager() {
-        NamedCommands.registerCommand("ShootAtSpeaker", new PPShootAtTarget(ShootTarget.SPEAKER).withTimeout(2));
-        NamedCommands.registerCommand("RunPickup", new RunPickup());
-        NamedCommands.registerCommand("LaunchNote", new LaunchNote());
+        NamedCommands.registerCommand("AutoEnableDefaults", new AutoEnableDefaults());
+        NamedCommands.registerCommand("PPShootAtTarget", new PPShootAtTarget().withTimeout(2));
         SwerveDrivetrain.getInstance().configurePathPlanner();
         autoChooser = AutoBuilder.buildAutoChooser();
         SmartDashboard.putData("Auto Mode", autoChooser);
