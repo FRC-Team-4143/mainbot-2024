5f813a671c4ea484cc0a224dc31fe75399e3d2fe
ColeHunt <huntcr2001@gmail.com>
2024-03-05 09:09:00 -0600

    adds status flag publisher and subscriber for vision reset control

--- src/main/java/frc/robot/subsystems/PoseEstimator.java
+++ src/main/java/frc/robot/subsystems/PoseEstimator.java
@@ -4,7 +4,8 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.BooleanSubscriber;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.ProtobufPublisher;
 import edu.wpi.first.networktables.ProtobufSubscriber;
@@ -12,16 +13,10 @@ import edu.wpi.first.networktables.TimestampedObject;
 
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
-import org.opencv.dnn.Net;
 
-import edu.wpi.first.math.MatBuilder;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.lib.Util;
 import frc.lib.subsystem.Subsystem;
 
 public class PoseEstimator extends Subsystem {
@@ -40,20 +35,14 @@ public class PoseEstimator extends Subsystem {
     private SwerveDrivePoseEstimator odometry_;
     private SwerveDrivePoseEstimator vision_filtered_odometry_;
     private ProtobufSubscriber<Pose2d> vision_subsciber_;
+    private BooleanPublisher supression_publisher_;
+    private BooleanSubscriber vision_ready_subscriber_;
     private ProtobufPublisher<Pose2d> odom_publisher_;
     private ProtobufPublisher<Pose2d> pose_publisher_;
-    private ProtobufPublisher<Pose2d> nvidia0_publisher;
-    private ProtobufPublisher<Pose2d> nvidia1_publisher;
-
-    private NetworkTable nvidia0_;
-    private NetworkTable nvidia1_;
-    private Transform2d nvidia0_transform_ = new Transform2d(new Translation2d(-0.35,0.35), Rotation2d.fromDegrees(0));//30+90));
-    private Transform2d nvidia1_transform_ = new Transform2d(new Translation2d(-0.35,0.35), Rotation2d.fromDegrees(0));//-30+90));
+   
     private Transform2d vision_box_transform_ = new Transform2d(new Translation2d(0.33,-0.457), Rotation2d.fromDegrees(180));//30+90));
-    private Pose2d nvidia0_result_pose_;
-    private Pose2d nvidia1_result_pose_;
-
-    int counter = 10;
+   
+    int update_counter_ = 10;
 
     PoseEstimator() {
         io_ = new PoseEstimatorPeriodicIoAutoLogged();
@@ -63,59 +52,30 @@ public class PoseEstimator extends Subsystem {
         var field_pose_topic = nti.getProtobufTopic("vision/pose", Pose2d.proto);
         var robot_odom_topic = nti.getProtobufTopic("vision/odom", Pose2d.proto);
         var robot_pose_topic = nti.getProtobufTopic("robotpose", Pose2d.proto);
+        var supress_odom_topic = nti.getBooleanTopic("vision/supress_odom");
+        var vision_ready_topic = nti.getBooleanTopic("vision/ready");
 
         vision_subsciber_ = field_pose_topic.subscribe(new Pose2d());
+        vision_ready_subscriber_ = vision_ready_topic.subscribe(false);
         odom_publisher_ = robot_odom_topic.publish();
         pose_publisher_ = robot_pose_topic.publish();
-
-        nvidia0_= NetworkTableInstance.getDefault().getTable("WarVision");
-        nvidia1_= NetworkTableInstance.getDefault().getTable("WarVision1");
-        var nvidia0_topic = nti.getProtobufTopic("vision/nvidia0", Pose2d.proto);
-        var nvidia1_topic = nti.getProtobufTopic("vision/nvidia1", Pose2d.proto);
-        nvidia0_publisher = nvidia0_topic.publish();
-        nvidia1_publisher = nvidia1_topic.publish(); 
-        }
+        supression_publisher_ = supress_odom_topic.publish(); 
+    }
 
     @Override
     public void reset() {
+        io_ = new PoseEstimatorPeriodicIoAutoLogged();
         odometry_ = new SwerveDrivePoseEstimator(SwerveDrivetrain.getInstance().kinematics, new Rotation2d(),
                 SwerveDrivetrain.getInstance().getModulePositions(), new Pose2d());
         vision_filtered_odometry_ = new SwerveDrivePoseEstimator(SwerveDrivetrain.getInstance().kinematics,
                 new Rotation2d(), SwerveDrivetrain.getInstance().getModulePositions(),
                 new Pose2d());
-        // vision_filtered_odometry.setVisionMeasurementStdDevs()
-        io_ = new PoseEstimatorPeriodicIoAutoLogged();
     }
 
     @Override
     public void readPeriodicInputs(double timestamp) {
         TimestampedObject<Pose2d> result = vision_subsciber_.getAtomic();
-        // Translation2d nvidia0_result_cord = new Translation2d(nvidia0_.getEntry("botposeX").getDouble(0), nvidia0_.getEntry("botposeY").getDouble(0));
-        // Translation2d nvidia1_result_cord = new Translation2d(nvidia1_.getEntry("botposeX").getDouble(0), nvidia1_.getEntry("botposeY").getDouble(0));
-        // Rotation2d nvidia0_result_rot = new Rotation2d(nvidia0_.getEntry("EulerAngleZ").getDouble(0)+Math.toDegrees(30+90));
-        // Rotation2d nvidia1_result_rot = new Rotation2d(nvidia1_.getEntry("EulerAngleZ").getDouble(0)+Math.toDegrees(-30+90));
-        // double nvidia0_num_dects = nvidia0_.getEntry("numdetections").getDouble(0);
-        // double nvidia1_num_dects = nvidia1_.getEntry("numdetections").getDouble(0);
-
-        // Pose2d nvidia0_result = new Pose2d(nvidia0_result_cord, nvidia0_result_rot);
-        // Pose2d nvidia1_result = new Pose2d(nvidia1_result_cord, nvidia1_result_rot);
-
-        // nvidia0_result_pose_ = nvidia0_result.transformBy(nvidia0_transform_);
-        // nvidia1_result_pose_ = nvidia1_result.transformBy(nvidia1_transform_);
-
-
-        // if (nvidia0_num_dects > 1){
-        //     vision_filtered_odometry_.addVisionMeasurement(nvidia0_result_pose_, 
-        //     timestamp - 0.03, 
-        //     new MatBuilder<>(Nat.N3(), Nat.N1()).fill(1/nvidia0_num_dects, 1/nvidia0_num_dects, 0.5/nvidia0_num_dects));
-        // }
-
-        // if (nvidia1_num_dects > 1){
-        //     vision_filtered_odometry_.addVisionMeasurement(nvidia1_result_pose_, 
-        //      timestamp - 0.03, 
-        //     new MatBuilder<>(Nat.N3(), Nat.N1()).fill(1/nvidia0_num_dects, 1/nvidia0_num_dects, 0.5/nvidia0_num_dects));
-        // }
-
+    
         if (result.timestamp > io_.last_vision_timestamp_) {
             vision_filtered_odometry_.addVisionMeasurement(result.value.transformBy(vision_box_transform_), timestamp - 0.2);
             io_.last_vision_timestamp_ = result.timestamp;
@@ -135,13 +95,11 @@ public class PoseEstimator extends Subsystem {
 
     @Override
     public void writePeriodicOutputs(double timestamp) {
-        counter--;
-        if(counter == 0){
+        update_counter_--;
+        if(update_counter_ == 0){
             odom_publisher_.set(io_.pose_);
-            counter = 10;
+            update_counter_ = 10;
         }
-        // nvidia0_publisher.set(nvidia0_result_pose_);
-        // nvidia1_publisher.set(nvidia1_result_pose_);
     }
 
     @Override
@@ -175,6 +133,7 @@ public class PoseEstimator extends Subsystem {
         Pose2d pose_ = new Pose2d();
         Pose2d vision_filtered_pose_ = new Pose2d();
         double last_vision_timestamp_ = 0.0;
+        boolean vision_supress_odom_ = true;
     }
 
     @Override
